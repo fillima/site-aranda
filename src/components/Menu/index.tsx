6f31e7d78b27949393db2713d5c8ee3e55e9a981
Filipe <filipelima849@gmail.com>
2023-07-11 17:45:54 -0300

    Adicionado link corretamente

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { Container, Empresa } from "./styles"
 
 export default function Menu() {
     return(
         <Container>
-            <Empresa><a href="/">Aranda</a></Empresa>
+            <Empresa><Link href="/">Aranda</Link></Empresa>
             <p>Gerador de vendas</p>
         </Container>
     );
