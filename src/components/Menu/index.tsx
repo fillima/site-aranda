ef91acc384f5726beb497da5fd433bd6c583a5ea
Filipe <filipelima849@gmail.com>
2023-07-20 13:17:02 -0300

    Ajustado botão de logout

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -60,7 +60,7 @@ export default function MenuAranda() {
                                     )}
                                     onClick={handleLogout}
                                 >
-                                    Sign out
+                                    Sair
                                 </button>
                                 )}
                             </Menu.Item>
